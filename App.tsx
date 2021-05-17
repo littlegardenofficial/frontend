7f7c5c27a7ef9c58f3be6b2e4544a8aaa201ff3b
mj07yadav <a9412446617526244@gmail.com>
2021-05-17 21:06:30 +0530

    "fixed drawer auto close when someone clicks out of the sidemenu "

--- App.tsx
+++ App.tsx
@@ -8,21 +8,21 @@ import {
 } from 'react-native-safe-area-context';
 import FlashMessage from 'react-native-flash-message';
 import SplashScreen from 'react-native-splash-screen';
-import { StatusBar } from 'react-native';
-import { THEME_COLOR } from './src/styles/theme';
+import {StatusBar} from 'react-native';
+import {THEME_COLOR} from './src/styles/theme';
 
-export interface Props {};
-export interface State {};
+export interface Props {}
+export interface State {}
 class App extends React.Component<Props, State> {
   render() {
     return (
       <Provider store={store}>
         <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-          <StatusBar 
-            animated={true} 
-            backgroundColor={THEME_COLOR} 
-            barStyle="light-content" 
-            hidden={false} 
+          <StatusBar
+            animated={true}
+            backgroundColor={THEME_COLOR}
+            barStyle="light-content"
+            hidden={false}
           />
           <Navigator />
           <FlashMessage position="top" style={{alignItems: 'center'}} />
